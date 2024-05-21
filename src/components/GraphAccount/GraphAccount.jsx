4e2f4235f5cf7325c8b7c9c55ed42c49143bdc24
Minsoo Cho <textign@gmail.com>
2024-05-21 17:25:01 +0900

    자잘한 수정

--- src/components/GraphAccount/GraphAccount.jsx
+++ src/components/GraphAccount/GraphAccount.jsx
@@ -7,6 +7,7 @@ const StGraphbox = styled.div`
   display: flex;
   justify-content: center;
   font-weight: 500;
+  font-size: 24px;
 `;
 function GraphAccount({ amount }) {
   return <StGraphbox>총 사용한 금액은 {amount}원 입니다.</StGraphbox>;
